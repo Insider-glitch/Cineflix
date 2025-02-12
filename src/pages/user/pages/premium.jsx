a013197dd5d317624b6e264618f516ea2da07ecb
Z <jjiyena6@gmail.com>
2025-02-12 13:20:38 +0800

    responsiveness

--- src/pages/user/pages/premium.jsx
+++ src/pages/user/pages/premium.jsx
@@ -38,7 +38,7 @@ const PremiumContent = () => {
           {loading ? (
             <p className="text-center text-lg">Loading movies...</p>
           ) : (
-            <div className="grid grid-cols-2 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+            <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
               {movies
                 .filter(
                   (item) =>
