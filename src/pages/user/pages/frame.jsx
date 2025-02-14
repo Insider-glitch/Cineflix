82a657ac1ff22c31ca2570a39c30764fb0672775
Z <jjiyena6@gmail.com>
2025-02-14 22:22:24 +0800

    fking nigga

--- src/pages/user/pages/frame.jsx
+++ src/pages/user/pages/frame.jsx
@@ -11,9 +11,15 @@ export default function Frame() {
 
   return (
     <div>
+      <div className="flex justify-center">
+        <img
+          className="m-4 block max-h-60 rounded-md bg-red-700 p-1 sm:hidden"
+          src={movie.image}
+        ></img>
+      </div>
       <div className="flex items-start justify-center p-4">
         <img
-          className="m-4 max-h-60 rounded-md bg-red-700 p-1"
+          className="m-4 hidden max-h-60 rounded-md bg-red-700 p-1 sm:block"
           src={movie.image}
         ></img>
         <div>
@@ -35,7 +41,7 @@ export default function Frame() {
           scrolling="no"
           allowFullScreen
           allow="fullscreen"
-          className="h-[80vh] w-[80vw] overflow-hidden border-none sm:h-[50vh] sm:w-[90vw] md:h-[60vh] md:w-[80vw] lg:h-[70vh] lg:w-[70vw]"
+          className="h-[40vh] w-[80vw] overflow-hidden border-none sm:h-[50vh] sm:w-[90vw] md:h-[60vh] md:w-[80vw] lg:h-[70vh] lg:w-[70vw]"
           title={`${movie.name || "Movie"}`}
         />
       </div>
